380de131f19d0f0057e480b09358594e83c7c61a
modichirag <modichirag@berkeley.edu>
2020-03-30 13:20:25 -0700

    update scrape all script with new codes

--- scraping/scrape_all.py
+++ scraping/scrape_all.py
@@ -5,12 +5,27 @@ import sys
 import pandas as pd
 import io
 
-import US_state_daily as US
+import US_state_daily as US_state
+import US_county_daily as US_county
 import Italy_daily as Italy
+import country_daily as world
+import China_jhu as China
+import Canada_jhu as Canada
+import Australia_jhu as Australia
 
 if __name__=="__main__":
 
-    print("Getting and saving US data")
-    US.scrape()
+    print("Getting and saving US state data")
+    US_state.scrape()
+    print("Getting and saving US county data")
+    US_county.scrape()
     print("Getting and saving Italy data")
     Italy.scrape()
+    print("Getting and saving World data from JHU website")
+    world.scrape()
+    print("Getting and saving China data from JHU website timeseries")
+    China.scrape()
+    print("Getting and saving Canada data from JHU website timeseries")
+    Canada.scrape()
+    print("Getting and saving Australia data from JHU website timeseries")
+    Australia.scrape()
