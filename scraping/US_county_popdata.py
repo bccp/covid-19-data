7338aee605861c279825e69a4d3534ba0eb79c89
modichirag <modichirag@berkeley.edu>
2020-03-30 19:43:13 -0700

    spelling error

--- scraping/US_county_popdata.py
+++ scraping/US_county_popdata.py
@@ -3,14 +3,15 @@ import pandas as pd
 
 def collect_and_save():
 
-    df1 = pd.read_csv('../data/US/US_county_pop_data.csv')
+    df1 = pd.read_csv('../data/US/US_county_population_estimate_data.csv')
     df2 = pd.read_csv('../data/US/US_county_area_data_2010.csv')
+
     df1cols = df1.columns
     df2cols = df2.columns
     cols_to_save = df2cols[[6, 4, 7, 8, 9, 11, 12, 13]]
     states = np.unique(df1['STNAME'].values)
     
-    colheader = ['state', 'county_name', 'fips', 'population_2010', 'housing_units', 'total_area_sqmiles', 'land_area_sqmiles', 'population_denisty', 'housing_denisty', 'population_2010_est', 'population_2019_est']
+    colheader = ['state', 'county_name', 'fips', 'population_2010', 'housing_units', 'total_area_sqmiles', 'land_area_sqmiles', 'population_density', 'housing_density', 'population_2010_est', 'population_2019_est']
     df = pd.DataFrame(columns=colheader)
 
     for iss, ss in enumerate(states):
